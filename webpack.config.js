482a15ce1d9f9d5c3a300b87aa78a37f1dd4ebbc
nikoloza <nika.tomadze@gmail.com>
2017-02-24 03:05:45 +0400

    added cargobase examples

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+var path = require('path')
+var webpack = require("webpack")
 var precss = require('precss')
-var cssimport = require('postcss-import')
+var postcssImport = require('postcss-import')
 var cssnext = require('postcss-cssnext')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
@@ -7,24 +9,61 @@ module.exports = {
   entry: './index.js',
   template: './index.html',
   output: {
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     loaders: [{
       test: /\.css$/,
       loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+    }, {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015'],
+        plugins: ['transform-class-properties']
+      }
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /.*\.(gif|png|jpe?g|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+      ]
     }]
   },
   postcss: function (webpack) {
     return [
-      precss,
-      cssimport({
+      postcssImport({
         addDependencyTo: webpack
       }),
+      precss,
       cssnext()
     ]
   },
+  devServer: {
+    historyApiFallback: true
+  },
   plugins: [
-    new ExtractTextPlugin('bundle.css')
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      children: true,
+      async: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // 'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
   ]
 }
